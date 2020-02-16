6db6c2e64aa311c360e86fbc4f0aff103a67944e
Mohamad Ramdan Firdaus <mohamad.firdaus@grabtaxi.com>
2020-02-16 16:59:50 +0700

    Simple navigation drawer

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <template>
-  <v-app>
+  <v-app>    
     <v-app-bar
       app
       color="primary"
       dark
     >
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
+
       <div class="d-flex align-center">
         <v-img
           alt="Vuetify Logo"
@@ -15,27 +17,11 @@
           width="40"
         />
 
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
+        <v-toolbar-title>Ramdan's Blog</v-toolbar-title>
       </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
     </v-app-bar>
+
+    <navigation-drawer :drawer="drawer"></navigation-drawer>    
 
     <v-content>
       <HelloWorld/>
@@ -45,16 +31,17 @@
 
 <script>
 import HelloWorld from './components/HelloWorld';
+import NavigationDrawer from './components/NavigationDrawer';
 
 export default {
   name: 'App',
-
   components: {
     HelloWorld,
+    NavigationDrawer,
   },
 
   data: () => ({
-    //
+    drawer: false,
   }),
 };
 </script>
---
+++ src/components/NavigationDrawer.vue
@@ -0,0 +1,49 @@
+<template>
+  <v-navigation-drawer
+    v-model="showDrawer"
+    absolute
+    bottom
+    temporary
+  >
+    <v-list
+      nav
+      dense
+    >
+      <v-list-item-group
+        active-class="deep-purple--text text--accent-4"
+      >
+        <v-list-item>
+          <v-list-item-title>Foo</v-list-item-title>
+        </v-list-item>
+
+        <v-list-item>
+          <v-list-item-title>Bar</v-list-item-title>
+        </v-list-item>
+
+        <v-list-item>
+          <v-list-item-title>Fizz</v-list-item-title>
+        </v-list-item>
+
+        <v-list-item>
+          <v-list-item-title>Buzz</v-list-item-title>
+        </v-list-item>
+      </v-list-item-group>
+    </v-list>
+  </v-navigation-drawer>
+</template>
+
+<script>
+  export default {
+    props: ['drawer'],
+    data: function () {
+      return {
+        showDrawer: this.drawer,
+      }
+    },
+    watch: {
+      drawer: function (newVal) {
+        this.showDrawer = newVal;
+      }
+    }
+  }
+</script>
