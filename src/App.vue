194f0d3bf7bd928a425ce7d50538a10e47fda73a
Mohamad Ramdan Firdaus <mohamad.firdaus@grabtaxi.com>
2020-02-17 06:39:30 +0700

    Separate toolbar and nav

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,5 @@
 <template>
   <v-app>    
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-toolbar-title>Ramdan's Blog</v-toolbar-title>
-      </div>
-    </v-app-bar>
-
-    <navigation-drawer></navigation-drawer>    
-
     <v-content>
       <router-view></router-view>
     </v-content>
@@ -28,15 +7,7 @@
 </template>
 
 <script>
-import NavigationDrawer from './components/NavigationDrawer';
-
 export default {
   name: 'App',
-  components: {
-    NavigationDrawer,
-  },
-  data: () => ({
-    // 
-  }),
 };
 </script>
---
+++ src/components/AppContainer.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <v-app-bar
+      app
+      color="primary"
+      dark
+    >
+      <div class="d-flex align-center">
+        <v-img
+          alt="Vuetify Logo"
+          class="shrink mr-2"
+          contain
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+          transition="scale-transition"
+          width="40"
+        />
+
+        <v-toolbar-title>Ramdan's Blog</v-toolbar-title>
+      </div>
+    </v-app-bar>
+    <navigation-drawer></navigation-drawer>
+  </div>
+</template>
+
+<script>
+import NavigationDrawer from './components/NavigationDrawer';
+
+export default {
+  components: {
+    NavigationDrawer,
+  }  
+}
+</script>
